7998a3df1777708aa9a64bd5eed5625bd3782c8f
Enis Sevim <esevim@unc.edu>
2023-09-11 15:06:42 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,15 +1,17 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730577037"
 
-word = (input("Enter a 5-character word: ")).lower()
+word: str = (input("Enter a 5-character word: "))
 if len(word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-single_chr = (input("Enter a single character: ")).lower()
+single_chr: str = (input("Enter a single character: "))
 if len(single_chr) != 1:
     print("Error: Character must be a single character.")
     exit()
 
+word = word.lower()
+single_chr = single_chr.lower()
 
 print("Searching for " + single_chr + " in " + word)
 
@@ -40,6 +42,9 @@ if single_chr == word[4]:
     count += 1
 
 if count == 0:
-    print(str(count) + " instance of " + single_chr + " found in " + word)
+    print("No instances of " + single_chr + " found in " + word)
 else:
-    print(str(count) + " instance of " + single_chr + " found in " + word)
+    if count == 1:
+        print(str(count) + " instance of " + single_chr + " found in " + word)
+    else:
+        print(str(count) + " instances of " + single_chr + " found in " + word)
