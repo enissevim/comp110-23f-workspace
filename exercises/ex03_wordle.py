424506347e8d4de4694ec5e576cfb0181a20dc3a
Enis Sevim <esevim@unc.edu>
2023-09-25 16:37:03 -0400

    Progress on Exercise 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -10,13 +10,14 @@ def contains_char(secret_word: str, char: str) -> bool:
     found: bool = False
     while idx < len(secret_word):
         if char == secret_word[idx]:
-            found = True   
+            found = True
         idx += 1
 
     if found is True:
         return True
     else:
-        return False    
+        return False
+
 
 # defining function to return the correct emoji's for every guess
 def emojified(guess: str, secret_word: str) -> str:
@@ -24,18 +25,18 @@ def emojified(guess: str, secret_word: str) -> str:
     assert len(guess) == len(secret_word)
     idx: int = 0
 
+
     secret_word_idx: int = 0
     emoji: str = ""
-
 
     # creating emoji's
     WHITE_BOX: str = "\U00002B1C"
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
 
-    while secret_word_idx < len(secret_word):
+    while idx < len(secret_word):
         # show green box
-        if guess[idx] == secret_word[secret_word_idx]:     
+        if guess[idx] == secret_word[idx]:
             emoji += GREEN_BOX
         else:
             # declare a boolean variable to keep track of whether the guessed character exists anywhere else in the word
@@ -47,23 +48,24 @@ def emojified(guess: str, secret_word: str) -> str:
             # white box
             else:
                 emoji += WHITE_BOX
-        
+
         idx += 1
-        secret_word_idx = 0
+        secret_word_idx += 1
 
     return emoji
 
 
 # defining function to ask for an input from the use
 def input_guess(length: int) -> str:
-    """Checking the lenght of the guess vs the length of the word."""
+    """Checking the length of the guess vs the length of the word."""
     guess: str = input(f"Enter a {length} character word: ")
-    
+
     # making sure the user is entering 5 letter words
     while len(guess) != length:
         guess = input(f"That wasn't {length} chars! Try again: ")
     else:
         return guess
+
 
 # declaring a function that includes the secret word and allows the program to start
 def main() -> None:
@@ -72,10 +74,10 @@ def main() -> None:
     turn: int = 1
     win: bool = False
 
-    # telling user which turn they are on
+    # telling the user which turn they are on
     while turn <= 6:
         if win is False:
-            print(f"=== Turn {turn}/6 ===") 
+            print(f"=== Turn {turn}/6 ===")
             new_guess: str = input_guess(len(secret_word))
             results = emojified(new_guess, secret_word)
             print(results)
@@ -84,10 +86,9 @@ def main() -> None:
                 win = True
         turn += 1
 
-    # showing user that the game ended
+    # showing the user that the game ended
     if win is not True:
         print("X/6 - Sorry try again tomorrow!")
-
 
 if __name__ == "__main__":
     main()
